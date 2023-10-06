eb52cd392c99d4af36e269e4ad2cfdb237da05d0
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-10-06 20:54:26 +0100

    refactor(github_actions): remove hardcoded thumprint
    reuse `github_thumbprints` variable, rather than having a hardcoded variable.

--- modules/github_action/main.tf
+++ modules/github_action/main.tf
@@ -111,7 +111,7 @@ resource "aws_iam_openid_connect_provider" "github_openid_connect" {
     "sts.amazonaws.com",
   ]
 
-  thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
+  thumbprint_list = var.github_thumbprints
 
   tags = merge(
     local.common_tags,

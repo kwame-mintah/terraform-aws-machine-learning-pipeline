95be026ba5441d73c3846a398ad3d80ddafcb0cd
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-11-15 20:37:38 +0000

    refactor(s3_bucket): remove bucket acls
    no longer needed as ownership controls have been changed to `BucketOwnerEnforced`

--- modules/s3_bucket/main.tf
+++ modules/s3_bucket/main.tf
@@ -40,13 +40,6 @@ resource "aws_s3_bucket" "s3_bucket" {
   })
 }
 
-resource "aws_s3_bucket_acl" "s3_bucket_acl" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  acl    = "private"
-
-  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
-}
-
 resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
   bucket = aws_s3_bucket.s3_bucket.id
 

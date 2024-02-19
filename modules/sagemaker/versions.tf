d90bc5120c0854d2df23b1ebe6d66735f6faa362
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-02-19 18:27:10 +0000

    feat(versions): update aws provider version to `5.37.0`
    project aws provider updated to latest 5.37.0 version and modules updated to reflect this new version.

--- modules/sagemaker/versions.tf
+++ modules/sagemaker/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.17.0"
+      version = "~> 5.37.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.5.1"
+      version = "~> 3.5.1"
     }
   }
 }
